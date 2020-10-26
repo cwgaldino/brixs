0211ca82b954a714fedf239ca077a051dcc12892
cwgaldino <carlosw.galdino@gmail.com>
2020-10-26 16:47:32 -0300

    documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,9 +48,19 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'nature'
+# html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- sphinx_copybutton configuration ------------------------------------------
+copybutton_prompt_text = ">>> "
+copybutton_remove_prompts = True
+copybutton_only_copy_prompt_lines = True
+
+# -- autodoc configuration ----------------------------------------------------
+autodoc_member_order = 'bysource'
