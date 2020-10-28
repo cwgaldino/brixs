2aff0b0c08defc71ee29f4f1824ff5135cf56609
cwgaldino <carlosw.galdino@gmail.com>
2020-10-28 14:38:32 -0300

    docs

--- brixs/brixs.py
+++ brixs/brixs.py
@@ -61,8 +61,8 @@ def simulate_spectrum(c, w, excitations, factor=1):
         >>> plt.ylabel('Intensity')
         >>> plt.show()
 
-        .. image:: /_figs/simulate_spectrum.png
-            :target: simulate_spectrum.png
+        .. image:: _figs/simulate_spectrum.png
+            :target: _static/simulate_spectrum.png
             :width: 400
             :align: center
 
