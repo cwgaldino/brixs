9404f300e40cd7bc50379d2fc158d305fa8b5c8d
cwgaldino <carlosw.galdino@gmail.com>
2021-10-31 11:00:20 +0100

    publish the docs

---
+++ backpack.sh
@@ -0,0 +1,5 @@
+cp ~/github/py-backpack/backpack/filemanip.py ~/github/brixs/brixs/
+cp ~/github/py-backpack/backpack/figmanip.py ~/github/brixs/brixs/
+cp ~/github/py-backpack/backpack/arraymanip.py ~/github/brixs/brixs/
+cp ~/github/py-backpack/backpack/intermanip.py ~/github/brixs/brixs/
+cp ~/github/py-backpack/backpack/model_functions.py ~/github/brixs/brixs/
